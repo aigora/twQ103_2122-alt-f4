ed811577eef9a3fbb5297589fa719c32bd660756
Mario Calderón <mario.calderon@alumnos.upm.es>
2022-05-24 16:09:14 +0200

    Update CargarPartida.c

--- CargarPartida.c
+++ CargarPartida.c
@@ -185,7 +185,7 @@ printf("Al llegar a la zona de basura, las puertas están bloqueadas y hay 3 guar
 printf("A)Disparáis a los guardas desde los tubos y utilizáis la sierra eléctrica para abrir un agujero la puerta.\n");
 printf("B)Bajáis de los tubos amenazándoles con matar al presidente si no os abren.\n");
 printf("C)Os entregáis y cuando os llevan fuera intentáis escapar.\n");
-printf("D)Echáis gas somnífero a los guardas y cuando estén dormidos utilizáis la sierra eléctrica para abrir un agujero la puerta \n");
+printf("D)Echáis gas somnífero a los guardas y cuando estén dormidos utilizáis la sierra eléctrica para abrir un agujero la puerta.\n");
 
 //Pregunta 13
 printf("Solo vuestros compañeros que están fuera saben que váis a salir por el sótano que hay tras la zona de la basura, os esperan ahí y huís:\n");
